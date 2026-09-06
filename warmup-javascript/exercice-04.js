2ee8952791ea1dc2ae6161fbc11f5dcdbe58496b
abdelilah abdi <abdelilahabdi6633@gmail.com>
2026-09-06 01:52:15 +0100

    ex 4 question 5 et bonus

--- warmup-javascript/exercice-04.js
+++ warmup-javascript/exercice-04.js
@@ -24,3 +24,62 @@ for (let i = 0 ; i < courses.length ; i++) {
   let num = courses[i];
   console.log(i + 1  + ". " + num) ;
 }
+
+
+
+
+
+
+//const courses = ["pain" , "riz" , "cafe" , "sucre"] ;
+//console.log(courses.includes("cafe"))
+if (courses.includes("cafe") ) {
+  console.log("Le cafe est bien dans la liste.") ;
+} 
+else {
+  console.log("non") ;
+}
+//console.log() ;
+
+
+
+
+
+
+//push()
+
+//tzid f lekeher element array
+
+//indexOf()
+//tl9a element dyal index
+
+//splice()
+//tmse7 element men array
+
+//length
+//t3rf 3la 3adad dyal elements
+
+//for + courses[i]
+
+//tdoz 3la 3anasir w te3redhom
+
+//includes()
+//tchof wach element kayen
+
+
+//bonus 
+
+//const courses = ["pain" , "riz" , "cafe" , "sucre"] ;
+
+console.log(courses.sort());
+console.log(courses[0]); //affich premier
+console.log(courses[courses.length - 1]); //affich la dernier
+
+// length hya che7al men 3onser or element 3endi
+// sort() hya t3rf 3la les elements w t3rfhom b ordre alphabétique
+// courses[0] hya t3rf 3la l element l awel
+// courses[courses.length - 1] hya t3rf 3la l element l a5er
+// includes() hya t3rf wach l element kayen f array wla la
+// indexOf() hya t3rf 3la l index dyal l element
+// splice() hya t3rf 3la l index dyal l element w tms7o
+// push() hya t3rf 3la l element w tzido f l a5er dyal l array
+// index hya fin kayen dak element or 3onser
